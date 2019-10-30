b4375a7d6b7c936a799ffa6bb963ce450fa27b89
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-10-30 09:11:24 -0600

    moved app.listen to server.js

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('common'));
+app.use(morgan('dev'));
 
 app.get('/', (req,res) => {
   res.send('hi express');
 });
 
 
-app.listen(8000, () => {
-  console.log('Server started on port 8000');
-});
+
+module.exports = app;
